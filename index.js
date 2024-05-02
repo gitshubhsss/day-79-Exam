fd8414ece0e1d3a0d5af437a12a4d5d11d334f06
gitshubhsss <shubhamranjane16@gmail.com>
2024-05-02 23:56:55 +0530

    java code for login page

---
+++ index.js
@@ -0,0 +1,32 @@
+// package com.example.loginapp;
+
+// import androidx.appcompat.app.AppCompatActivity;
+
+// import android.os.Bundle;
+// import android.view.View;
+// import android.widget.Button;
+// import android.widget.EditText;
+// import android.widget.Toast;
+
+// public class MainActivity extends AppCompatActivity {
+
+//     private EditText username,mobile_no,email,address;
+
+//     private Button button;
+
+
+//     @Override
+//     protected void onCreate(Bundle savedInstanceState) {
+//         super.onCreate(savedInstanceState);
+//         setContentView(R.layout.activity_main);
+
+//         button.findViewById(R.id.button);
+
+//         button.setOnClickListener(new View.OnClickListener() {
+//             @Override
+//             public void onClick(View v) {
+//                 Toast.makeText(MainActivity.this, "Form submited successfully", Toast.LENGTH_SHORT).show();
+//             }
+//         });
+//     }
+// }
